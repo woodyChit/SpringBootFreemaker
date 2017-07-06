8845f1f1ac2354225f14a7cbe57eb27837a75e3e
woody <woody_me@qq.com>
2017-07-06 21:39:59 +0800

    add redisTemplate valueOps

--- src/main/java/com/wd/Application.java
+++ src/main/java/com/wd/Application.java
@@ -1,5 +1,8 @@
 package com.wd;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wd.init.MySetting;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -12,6 +15,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -35,10 +41,28 @@ public class Application {
     }
 
 
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        setRedisTemplateValueSerializer(redisTemplate);
+        return redisTemplate;
 
+    }
     @Bean
     public MySetting setting(){
         return new MySetting();
     }
 
+
+    private void setRedisTemplateValueSerializer(RedisTemplate template){
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        ObjectMapper mapper= new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+        template.setKeySerializer(template.getStringSerializer());
+        template.setValueSerializer(serializer);
+
+    }
 }
--- src/main/java/com/wd/redis/RedisService.java
+++ src/main/java/com/wd/redis/RedisService.java
@@ -2,6 +2,7 @@ package com.wd.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,11 +10,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RedisService {
+    //only valueOps
+    //
 
-//    @Autowired
-//    RedisTemplate<String, ? > redisTemplate;
-//
-//    public void setValue(String key,Object o){
-//        redisTemplate.
-//    }
+    @Autowired
+    RedisTemplate<String,Object> redisTemplate;
+
+
+    public void setValue(String key,Object o){
+        ValueOperations<String,Object> ops = redisTemplate.opsForValue();
+        ops.set(key,o);
+
+    }
+
+    public <T>  T getValue(String key,Class<T> clazz){
+        ValueOperations<String,Object> ops = redisTemplate.opsForValue();
+        Object value = ops.get(key);
+        return clazz.cast(value);
+    }
 }
