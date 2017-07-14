b26c77ad1635db456f44ab1fd7f16ae29ed6267e
wd <wd@datacastle.cn>
2017-07-14 10:07:37 +0800

    freemaker suffix -> html

--- src/main/java/com/wd/controller/MainController.java
+++ src/main/java/com/wd/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -18,10 +19,12 @@ public class MainController {
     private Holder holder;
     @Autowired
     private MySetting setting;
-    @RequestMapping("/index")
-    public String index(Map<String,Object> map, @RequestParam String name){
-        map.put("name",holder.toString() + "+"+name);
-        return "index";
+
+    @RequestMapping("/index.html")
+    public ModelAndView index(ModelAndView modelAndView){
+        modelAndView.addObject("name",holder.toString()  );
+        modelAndView.setViewName("index");
+        return modelAndView;
 
     }
 
