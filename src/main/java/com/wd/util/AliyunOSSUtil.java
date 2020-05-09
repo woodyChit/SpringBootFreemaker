5ce770f40ee8a7308a8a436e722fb6d194ca5a47
woody <woody_me@qq.com>
2020-05-09 15:58:43 +0800

    Update AliyunOSSUtil.java

--- src/main/java/com/wd/util/AliyunOSSUtil.java
+++ src/main/java/com/wd/util/AliyunOSSUtil.java
@@ -6,10 +6,10 @@ import com.aliyun.oss.OSSClient;
  * Created by woody on 2017/8/6.
  */
 public class AliyunOSSUtil {
-    private final static String ENDPOINT= "oss-cn-beijing.aliyuncs.com";
-    private final static String ACCESS_KEY_ID = "LTAIvL3yiEW9YKve";
-    private final static String ACCESS_KEY_SECRET = "A7z0KyO5vJG44JKulzgQM7ryb";
-    private final static String BUCKET_NAME = "wdbucket199102Nav3c";
+    private final static String ENDPOINT= "XXXncs.com";
+    private final static String ACCESS_KEY_ID = "XXXX
+    private final static String ACCESS_KEY_SECRET = "XXXQM7ryb";
+    private final static String BUCKET_NAME = "XXXav3c";
 
 
     public static OSSClient getInstance(){
